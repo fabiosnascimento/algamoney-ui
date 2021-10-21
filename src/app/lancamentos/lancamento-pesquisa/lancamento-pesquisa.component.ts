ca80159db30c1e0cacfb89040801551c2c68de8a
Fabio Nascimento <fabiogtr@hotmail.com>
2021-10-21 11:54:00 -0300

    Implementando a paginação no serviço de lançamentos

--- src/app/lancamentos/lancamento-pesquisa/lancamento-pesquisa.component.ts
+++ src/app/lancamentos/lancamento-pesquisa/lancamento-pesquisa.component.ts
@@ -10,9 +10,7 @@ import { Component, OnInit } from '@angular/core';
 
 export class LancamentoPesquisaComponent implements OnInit {
 
-  descricao: string = '';
-  dataVencimentoInicio?: Date;
-  dataVencimentoFim?: Date;
+  filtro = new LancamentoFiltro();
   lancamentos: any = [];
 
   constructor(private lancamentoService: LancamentoService) {}
@@ -22,13 +20,9 @@ export class LancamentoPesquisaComponent implements OnInit {
   }
 
   pesquisar() {
-    const filtro: LancamentoFiltro = {
-      descricao: this.descricao,
-      dataVencimentoInicio: this.dataVencimentoInicio,
-      dataVencimentoFim: this.dataVencimentoFim
-    }
-
-    this.lancamentoService.pesquisar(filtro)
-      .then(lancamentos => this.lancamentos = lancamentos);
+    this.lancamentoService.pesquisar(this.filtro)
+      .then(resultado => {
+        this.lancamentos = resultado.lancamentos;
+      });
   }
 }
--- src/app/lancamentos/lancamento.service.ts
+++ src/app/lancamentos/lancamento.service.ts
@@ -2,10 +2,12 @@ import { DatePipe } from '@angular/common';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-export interface LancamentoFiltro {
-  descricao: string;
+export class LancamentoFiltro {
+  descricao?: string;
   dataVencimentoInicio?: Date;
   dataVencimentoFim?: Date;
+  pagina = 0;
+  itensPorPagina = 5;
 }
 
 @Injectable({
@@ -26,6 +28,9 @@ export class LancamentoService {
 
       let params = new HttpParams();
 
+      params = params.set('page', filtro.pagina);
+      params = params.set('size', filtro.itensPorPagina);
+
       if (filtro.descricao) {
         params = params.set('descricao', filtro.descricao);
       }
@@ -40,6 +45,15 @@ export class LancamentoService {
 
     return this.http.get(`${this.lancamentosUrl}?resumo`, { headers, params })
       .toPromise()
-      .then((response: any) => response['content']);
+      .then((response: any) => {
+        const lancamentos = response['content'];
+
+        const resultado = {
+          lancamentos,
+          total: response['totalElements']
+        };
+
+        return resultado;
+      })
   }
 }
