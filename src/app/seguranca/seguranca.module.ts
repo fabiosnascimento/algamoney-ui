4d29dafa28f2ed107c927459bc6c176726f82699
Fabio Nascimento <fabiogtr@hotmail.com>
2021-10-29 14:37:39 -0300

    Interceptando chamadas HTTP para tratar a expiração do access token

--- src/app/seguranca/seguranca.module.ts
+++ src/app/seguranca/seguranca.module.ts
@@ -1,3 +1,4 @@
+import { MoneyHttpInterceptor } from './money-http-interceptor';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -8,6 +9,7 @@ import { JwtModule, JwtHelperService } from '@auth0/angular-jwt';
 
 import { LoginFormComponent } from './login-form/login-form.component';
 import { SegurancaRoutingModule } from './seguranca-routing.module';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 export function tokenGetter(): string {
   return localStorage.getItem('token');
@@ -36,6 +38,11 @@ export function tokenGetter(): string {
   ],
   providers: [
     JwtHelperService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: MoneyHttpInterceptor,
+      multi: true
+    }
   ]
 })
 export class SegurancaModule {
